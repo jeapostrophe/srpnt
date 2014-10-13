38c901809a5b29a222b8e4c7ce62ebe73d791c10
Jay McCarthy <jay.mccarthy@gmail.com>
2014-10-12 20:28:30 -0400

    cleanup

--- s16vec-play.rkt
+++ s16vec-play.rkt
@@ -1,54 +1,67 @@
 #lang racket/base
-;; XXX copied from portaudio
-(require ffi/unsafe
-         (only-in '#%foreign ffi-callback)
-         portaudio/portaudio
+(require portaudio/portaudio
          portaudio/devices
-         racket/bool
-         racket/math)
+         racket/fixnum
+         racket/contract/base)
 
 (define channels 2)
 (define reasonable-latency 0.1)
+(define sample-rate 44100)
+(define sample-rate.0 (fx->fl sample-rate))
+(define samples-per-buffer (fxquotient sample-rate 60))
+(define frames-per-buffer (fx* channels samples-per-buffer))
 
-;; given an bytes, a starting frame, a stopping frame or
-;; false, and a sample rate, play the sound.
-(define (bytes-play bs start-frame pre-stop-frame sample-rate)
-  (define total-frames (/ (bytes-length bs) channels))
-  (define stop-frame (or pre-stop-frame
-                         total-frames))
-  (define sound-frames (- stop-frame start-frame))
+(define (make-buffer)
+  (make-bytes (* channels frames-per-buffer)))
+(define (make-bytes-player)
   (pa-maybe-initialize)
-  (define sr/i (exact->inexact sample-rate))
   (define device-number (find-output-device reasonable-latency))
   (define device-latency (device-low-output-latency device-number))
   (define output-stream-parameters
     (make-pa-stream-parameters
-     device-number ;; device
-     2             ;; channels
-     '(paUInt8)    ;; sample format
-     device-latency ;; latency
-     #f))            ;; host-specific info
+     device-number channels
+     '(paUInt8)
+     device-latency #f))
   (define stream
     (pa-open-stream
-     #f            ;; input parameters
-     output-stream-parameters
-     sr/i
-     0             ;; frames-per-buffer
-     '()           ;; stream-flags
-     #f
-     #f))
+     #f output-stream-parameters
+     sample-rate.0 frames-per-buffer null #f #f))
   (pa-start-stream stream)
-  (pa-write-stream stream bs total-frames)
-  (pa-close-stream stream))
+  stream)
+(define (bytes-play! bp bs)
+  (pa-write-stream bp bs frames-per-buffer))
+(define (close-bytes-player! bp)
+  (pa-close-stream bp))
+
+(provide
+ (contract-out
+  [make-buffer (-> bytes?)]
+  [make-bytes-player (-> stream?)]
+  [bytes-play! (-> stream? bytes? void?)]
+  [close-bytes-player! (-> stream? void?)]))
 
 (module+ main
-  (define v (make-bytes (* channels 44100)))
-  (for ([i (in-range 44100)])
-    (define sample
-      (max 0 (inexact->exact (round (* 128 (* 0.2 (sin (* (/ 1 44100) pi 2 i 302))))))))
-    (bytes-set! v (* i channels) sample)
-    (bytes-set! v (add1 (* i channels)) sample))
-
-  (printf "1-second tone at 302 Hz\n")
-  (bytes-play v 0 #f 44100)
+  (require racket/math
+           racket/flonum)
+  (define v (make-buffer))
+  (define bp (make-bytes-player))
+  (printf "1-second tone at 440 Hz\n")
+  (for ([s (in-range 60)])
+    (for ([j (in-range frames-per-buffer)])
+      (define i (fx+ (fx* frames-per-buffer s) j))
+      (define sample
+        (fxmax
+         0
+         (fl->fx
+          (flround
+           (fl* 128.0
+                (fl* 0.2
+                     (flsin (fl* (fl/ (fl* pi 2.0)
+                                      sample-rate.0)
+                                 (fl* (fx->fl i)
+                                      440.0)))))))))
+      (bytes-set! v (fx+ 0 (fx* j channels)) sample)
+      (bytes-set! v (fx+ 1 (fx* j channels)) sample))
+    (bytes-play! bp v))
+  (close-bytes-player! bp)
   (printf "...stop.\n"))
