dde54cc42e5b088b4fd76a5a1c582083d50f4d2e
Jay McCarthy <jay.mccarthy@gmail.com>
2014-10-20 17:09:12 -0400

    encoding a bunch of music theory

--- srpnt/examples/song.rkt
+++ srpnt/examples/song.rkt
@@ -286,6 +286,149 @@
   (beat-accents ts:3:4)
   (beat-accents ts:4:4))
 
+(define tone-names
+  '(C C# D D# E F F# G G# A A# B))
+(define tone->idx
+  (for/hasheq ([t (in-list tone-names)]
+               [i (in-naturals)])
+    (values t i)))
+
+(define (list-rotate base start)
+  (define len (length base))
+  (for/list ([i (in-range len)])
+    (define new-i (fx+ start i))
+    (define idx (fxmodulo new-i len))
+    (define doctave (fxquotient new-i len))
+    (match (list-ref base idx)
+      [(? symbol? tone)
+       (cons tone doctave)]
+      [(cons tone doctave1)
+       (cons tone (+ doctave1 doctave))])))
+
+(define (scale-chromatic/idx start-idx)
+  (list-rotate tone-names start-idx))
+(define (scale-chromatic start-tone)
+  (scale-chromatic/idx (hash-ref tone->idx start-tone)))
+(module+ test
+  (check-equal? (scale-chromatic/idx 0)
+                '((C . 0) (C# . 0) (D . 0) (D# . 0) (E . 0) (F . 0)
+                  (F# . 0) (G . 0) (G# . 0) (A . 0) (A# . 0) (B . 0)))
+  (check-equal? (scale-chromatic 'C#)
+                '((C# . 0) (D . 0) (D# . 0) (E . 0) (F . 0) (F# . 0)
+                  (G . 0) (G# . 0) (A . 0) (A# . 0) (B . 0) (C . 1))))
+
+(define (list-read l gaps)
+  (match gaps
+    ['()
+     '()]
+    [(cons gap gaps)
+     (cons (first l)
+           (list-read (list-tail l gap) gaps))]))
+
+(define scales (list scale-chromatic))
+
+(define (snoc l x) (append l (list x)))
+(define-syntax-rule (define-scale scale-name offsets)
+  (begin
+    (define (scale-name start-tone)
+      (define base (scale-chromatic start-tone))
+      (list-read base offsets))
+    (set! scales (snoc scales scale-name))))
+
+(define-scale scale-diatonic-major '(2 2 1 2 2 2 1))
+(module+ test
+  (check-equal? (scale-diatonic-major 'C)
+                '((C . 0) (D . 0) (E . 0) (F . 0) (G . 0) (A . 0) (B . 0)))
+  (check-equal? (scale-diatonic-major 'E)
+                '((E . 0) (F# . 0) (G# . 0) (A . 0) (B . 0) (C# . 1) (D# . 1))))
+
+(define-scale scale-natural-minor '(2 1 2 2 1 2 2))
+(define-scale scale-melodic-minor '(2 1 2 2 2 2 1))
+(define-scale scale-harmonic-minor '(2 1 2 2 1 3 1))
+(define-scale scale-diminished '(2 1 2 1 2 1 2 1))
+(define-scale scale-whole-tone '(2 2 2 2 2 2))
+(define-scale scale-blues '(3 2 1 1 3 2))
+(define-scale scale-minor-pentatonic '(3 2 2 3 2))
+(define-scale scale-major-pentatonic '(2 2 3 2 3))
+(define-scale scale-hungarian-minor '(2 1 3 1 1 3 1))
+(define-scale scale-persian '(1 3 1 1 2 3 1))
+;; XXX only has 5 so messes with chords
+;; (define-scale scale-hirojoshi '(2 1 4 1 4))
+(define-scale scale-arabian '(2 2 1 1 2 2 2))
+;; XXX only has 5 so messes with chords
+;; (define-scale scale-scottish '(2 3 2 2 3))
+;; xxx add exotic scales? http://www.lotusmusic.com/lm_exoticscales.html
+
+(module+ test
+  (for ([s (in-list scales)])
+    (printf "~a ~a: ~a\n" s 'C (s 'C))))
+
+(define (mode scale-tones start)
+  (list-rotate scale-tones start))
+
+(define modes empty)
+(define-syntax-rule (define-mode mode-name start)
+  (begin
+    (define (mode-name scale) (mode scale start))
+    (set! modes (snoc modes mode-name))))
+
+(define-mode mode-ionian 0)
+(define-mode mode-dorian 1)
+(define-mode mode-phrygian 2)
+(define-mode mode-lydian 3)
+(define-mode mode-mixolydian 4)
+(define-mode mode-aeolian 5)
+(define-mode mode-locrian 6)
+
+(module+ test
+  (check-equal? (mode-ionian (scale-diatonic-major 'C))
+                (scale-diatonic-major 'C))
+  (check-equal? (mode-lydian (scale-diatonic-major 'C))
+                '((F . 0) (G . 0) (A . 0) (B . 0) (C . 1) (D . 1) (E . 1))))
+
+(define (modes-of scale-tones)
+  (for/list ([i (in-range (length scale-tones))])
+    (mode scale-tones i)))
+
+(define (chord-triad scale-tones)
+  (list-read scale-tones '(2 2 0)))
+(module+ test
+  (check-equal? (chord-triad (scale-diatonic-major 'C))
+                '((C . 0) (E . 0) (G . 0))))
+
+(define (chord-seventh scale-tones)
+  (list-read scale-tones '(2 2 2 0)))
+(module+ test
+  (check-equal? (chord-seventh (scale-diatonic-major 'C))
+                '((C . 0) (E . 0) (G . 0) (B . 0))))
+
+(define (chord-sixth scale-tones)
+  (list-read scale-tones '(2 2 1 0)))
+(module+ test
+  (check-equal? (chord-sixth (scale-diatonic-major 'C))
+                '((C . 0) (E . 0) (G . 0) (A . 0))))
+
+(define (all-chords chord-kind scale-tones)
+  (map chord-kind (modes-of scale-tones)))
+
+(module+ test
+  (let ()
+    (define (random-list-ref l)
+      (list-ref l (random (length l))))
+    (define start (random-list-ref tone-names))
+    (define scale (random-list-ref scales))
+    (define scale-tones (scale start))
+    (printf "~a ~a: ~a\n" start scale scale-tones)
+    (printf "Triads:\n")
+    (for ([t (in-list (all-chords chord-triad scale-tones))])
+      (printf "\t~a\n" t))
+    (printf "Sevenths:\n")
+    (for ([t (in-list (all-chords chord-seventh scale-tones))])
+      (printf "\t~a\n" t))
+    (printf "Sixths:\n")
+    (for ([t (in-list (all-chords chord-sixth scale-tones))])
+      (printf "\t~a\n" t))))
+
 ;; xxx add different kinds of accents
 ;; xxx generate drum beats
 ;;  - https://en.wikipedia.org/wiki/Drum_beat from accents & time-sigs
