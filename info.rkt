effea53f5a2749fe84e631f5ba2e8a4b625678d8
Jay McCarthy <jay.mccarthy@gmail.com>
2015-07-29 10:07:37 -0600

    deps

--- info.rkt
+++ info.rkt
@@ -1,5 +1,8 @@
 #lang info
 (define collection 'multi)
-(define deps '("data-enumerate-lib"
+(define deps '("lux"
+               "lux-charterm"
+               "data-enumerate-lib"
                "base" "portaudio" "gui-lib" "rackunit-lib" "math-lib"))
-(define build-deps '("rackunit-lib" "plot-gui-lib"))
+(define build-deps '("plot-lib"
+                     "rackunit-lib" "plot-gui-lib"))
