b200b7a35294c5e4c045e5bcec30fc1b73743e63
Jay McCarthy <jay.mccarthy@gmail.com>
2019-12-19 17:00:25 -0500

    up

--- jena/synth.c
+++ jena/synth.c
@@ -1,58 +1,16 @@
-/*
- * $Id$
- *
- * This program uses the PortAudio Portable Audio Library.
- * For more information see: http://www.portaudio.com/
- * Copyright (c) 1999-2000 Ross Bencina and Phil Burk
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files
- * (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge,
- * publish, distribute, sublicense, and/or sell copies of the Software,
- * and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
- * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
- * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
-/*
- * The text above constitutes the entire PortAudio license; however,
- * the PortAudio community also makes the following non-binding requests:
- *
- * Any person wishing to distribute modifications to the Software is
- * requested to send the modifications to the original developer so that
- * they can be incorporated into the canonical version. It is also
- * requested that these non-binding requests be included along with the
- * license above.
- */
+#include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 #include <portaudio.h>
 
-#define NUM_SECONDS   (5)
-#define SAMPLE_RATE   (44100)
-#define FRAMES_PER_BUFFER  (64)
+#define NUM_SECONDS (5)
+#define SAMPLE_RATE (44100)
+#define FRAMES_PER_BUFFER (SAMPLE_RATE/60)
 
-#ifndef M_PI
-#define M_PI  (3.14159265)
-#endif
-
-#define TABLE_SIZE   (200)
+#define TABLE_SIZE (200)
 typedef struct {
   float sine[TABLE_SIZE];
-  int left_phase;
-  int right_phase;
-  char message[20]; }
+  int left_phase;}
   paTestData;
 
 /* This routine will be called by the PortAudio engine when audio is needed.
@@ -66,86 +24,57 @@ static int patestCallback( const void *inputBuffer, void *outputBuffer,
                            void *userData ) {
   paTestData *data = (paTestData*)userData;
   float *out = (float*)outputBuffer;
-  unsigned long i;
 
-  (void) timeInfo; /* Prevent unused variable warnings. */
+  /* Prevent unused variable warnings. */
+  (void) timeInfo;
   (void) statusFlags;
   (void) inputBuffer;
 
-  for( i=0; i<framesPerBuffer; i++ ) {
+  for( unsigned long i = 0; i < framesPerBuffer; i++ ) {
     *out++ = data->sine[data->left_phase];  /* left */
-    *out++ = data->sine[data->right_phase];  /* right */
     data->left_phase += 1;
-    if( data->left_phase >= TABLE_SIZE ) data->left_phase -= TABLE_SIZE;
-    data->right_phase += 3; /* higher pitch so we can distinguish left and right. */
-    if( data->right_phase >= TABLE_SIZE ) data->right_phase -= TABLE_SIZE; }
+    if( data->left_phase >= TABLE_SIZE ) data->left_phase -= TABLE_SIZE; }
 
   return paContinue; }
 
-static void StreamFinished( void* userData ) {
-  paTestData *data = (paTestData *) userData;
-  printf( "Stream Completed: %s\n", data->message ); }
-
-/*******************************************************************/
-int main(void) {
-  PaStreamParameters outputParameters;
-  PaStream *stream;
-  PaError err;
-  paTestData data;
-  int i;
-
-  printf("PortAudio Test: output sine wave. SR = %d, BufSize = %d\n", SAMPLE_RATE, FRAMES_PER_BUFFER);
-
-  /* initialise sinusoidal wavetable */
-  for( i=0; i<TABLE_SIZE; i++ ) {
-    data.sine[i] = (float) sin( ((double)i/(double)TABLE_SIZE) * M_PI * 2. ); }
-  data.left_phase = data.right_phase = 0;
-
-  err = Pa_Initialize();
-  if( err != paNoError ) goto error;
-
-  outputParameters.device = Pa_GetDefaultOutputDevice(); /* default output device */
-  if (outputParameters.device == paNoDevice) {
-    fprintf(stderr,"Error: No default output device.\n");
-    goto error; }
-  outputParameters.channelCount = 2;       /* stereo output */
-  outputParameters.sampleFormat = paFloat32; /* 32 bit floating point output */
-  outputParameters.suggestedLatency = Pa_GetDeviceInfo( outputParameters.device )->defaultLowOutputLatency;
-  outputParameters.hostApiSpecificStreamInfo = NULL;
-
-  err = Pa_OpenStream(&stream,
-                      NULL, /* no input */
-                      &outputParameters,
-                      SAMPLE_RATE,
-                      FRAMES_PER_BUFFER,
-                      paClipOff,      /* we won't output out of range samples so don't bother clipping them */
-                      patestCallback,
-                      &data );
-  if( err != paNoError ) goto error;
-
-  sprintf( data.message, "No Message" );
-  err = Pa_SetStreamFinishedCallback( stream, &StreamFinished );
-  if( err != paNoError ) goto error;
-
-  err = Pa_StartStream( stream );
-  if( err != paNoError ) goto error;
-
-  printf("Play for %d seconds.\n", NUM_SECONDS );
-  Pa_Sleep( NUM_SECONDS * 1000 );
-
-  err = Pa_StopStream( stream );
-  if( err != paNoError ) goto error;
-
-  err = Pa_CloseStream( stream );
-  if( err != paNoError ) goto error;
-
-  Pa_Terminate();
-  printf("Test finished.\n");
-
-  return err;
- error:
+void maybeDie( PaError err ) {
+  if ( err == paNoError ) { return; }
   Pa_Terminate();
   fprintf( stderr, "An error occured while using the portaudio stream\n" );
   fprintf( stderr, "Error number: %d\n", err );
   fprintf( stderr, "Error message: %s\n", Pa_GetErrorText( err ) );
-  return err; }
+  exit(err); }
+
+int main(void) {
+  printf("PortAudio Test: output sine wave. SR = %d, BufSize = %d\n", SAMPLE_RATE, FRAMES_PER_BUFFER);
+
+  /* initialise sinusoidal wavetable */
+  paTestData data;
+  for( int i = 0; i < TABLE_SIZE; i++ ) {
+    data.sine[i] = (float) sin( ((double)i/(double)TABLE_SIZE) * M_PI * 2. ); }
+  data.left_phase = 0;
+
+  maybeDie( Pa_Initialize() );
+
+  PaStreamParameters outputParameters;
+  outputParameters.device = Pa_GetDefaultOutputDevice(); /* default output device */
+  if (outputParameters.device == paNoDevice) {
+    fprintf(stderr,"Error: No default output device.\n");
+    exit(1); }
+  outputParameters.channelCount = 1;
+  outputParameters.sampleFormat = paFloat32;
+  outputParameters.suggestedLatency = Pa_GetDeviceInfo( outputParameters.device )->defaultLowOutputLatency;
+  outputParameters.hostApiSpecificStreamInfo = NULL;
+
+  PaStream *stream;
+  maybeDie( Pa_OpenStream(&stream, NULL, &outputParameters, SAMPLE_RATE, FRAMES_PER_BUFFER, paClipOff, patestCallback, &data ) );
+  maybeDie( Pa_StartStream( stream ) );
+
+  printf("Play for %d seconds.\n", NUM_SECONDS );
+  Pa_Sleep( NUM_SECONDS * 1000 );
+
+  maybeDie( Pa_StopStream( stream ) );
+  maybeDie( Pa_CloseStream( stream ) );
+  Pa_Terminate();
+  
+  return 0; }
