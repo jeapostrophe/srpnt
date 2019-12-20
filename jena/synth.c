b0f90f17d906408e25415f797cfe4f133ac4731a
Jay McCarthy <jay.mccarthy@gmail.com>
2019-12-19 21:42:31 -0500

    stream

--- jena/synth.c
+++ jena/synth.c
@@ -6,36 +6,7 @@
 #define NUM_SECONDS (5)
 #define SAMPLE_RATE (44100)
 #define FRAMES_PER_BUFFER (SAMPLE_RATE/60)
-
 #define TABLE_SIZE (200)
-typedef struct {
-  float sine[TABLE_SIZE];
-  int left_phase;}
-  paTestData;
-
-/* This routine will be called by the PortAudio engine when audio is needed.
-** It may called at interrupt level on some machines so don't do anything
-** that could mess up the system like calling malloc() or free().
-*/
-static int patestCallback( const void *inputBuffer, void *outputBuffer,
-                           unsigned long framesPerBuffer,
-                           const PaStreamCallbackTimeInfo* timeInfo,
-                           PaStreamCallbackFlags statusFlags,
-                           void *userData ) {
-  paTestData *data = (paTestData*)userData;
-  float *out = (float*)outputBuffer;
-
-  /* Prevent unused variable warnings. */
-  (void) timeInfo;
-  (void) statusFlags;
-  (void) inputBuffer;
-
-  for( unsigned long i = 0; i < framesPerBuffer; i++ ) {
-    *out++ = data->sine[data->left_phase];  /* left */
-    data->left_phase += 1;
-    if( data->left_phase >= TABLE_SIZE ) data->left_phase -= TABLE_SIZE; }
-
-  return paContinue; }
 
 void maybeDie( PaError err ) {
   if ( err == paNoError ) { return; }
@@ -46,13 +17,9 @@ void maybeDie( PaError err ) {
   exit(err); }
 
 int main(void) {
+  float buf[FRAMES_PER_BUFFER];
+  
   printf("PortAudio Test: output sine wave. SR = %d, BufSize = %d\n", SAMPLE_RATE, FRAMES_PER_BUFFER);
-
-  /* initialise sinusoidal wavetable */
-  paTestData data;
-  for( int i = 0; i < TABLE_SIZE; i++ ) {
-    data.sine[i] = (float) sin( ((double)i/(double)TABLE_SIZE) * M_PI * 2. ); }
-  data.left_phase = 0;
 
   maybeDie( Pa_Initialize() );
 
@@ -67,12 +34,18 @@ int main(void) {
   outputParameters.hostApiSpecificStreamInfo = NULL;
 
   PaStream *stream;
-  maybeDie( Pa_OpenStream(&stream, NULL, &outputParameters, SAMPLE_RATE, FRAMES_PER_BUFFER, paClipOff, patestCallback, &data ) );
+  maybeDie( Pa_OpenStream(&stream, NULL, &outputParameters, SAMPLE_RATE, FRAMES_PER_BUFFER, paClipOff, NULL, NULL ) );
   maybeDie( Pa_StartStream( stream ) );
 
   printf("Play for %d seconds.\n", NUM_SECONDS );
-  Pa_Sleep( NUM_SECONDS * 1000 );
-
+  int phase = 0;
+  for ( int i = 0; i < NUM_SECONDS*60; i++ ) {
+    for ( int j = 0; j < FRAMES_PER_BUFFER; j++ ) {
+      buf[j] = (float) sin( ((double)phase/(double)TABLE_SIZE) * M_PI * 2. );
+      phase++;
+      if( phase >= TABLE_SIZE ) { phase -= TABLE_SIZE; } }
+    maybeDie( Pa_WriteStream( stream, buf, FRAMES_PER_BUFFER ) ); }
+  
   maybeDie( Pa_StopStream( stream ) );
   maybeDie( Pa_CloseStream( stream ) );
   Pa_Terminate();
